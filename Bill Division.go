271e19747fda80bf3b4f296e8a8407c01c36e1e0
Sirojiddin-Kh <sirojiddinhamidov2001@gmail.com>
2021-06-22 12:42:29 +0500

    Bill Division

---
+++ Bill Division.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'bonAppetit' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY bill
+ *  2. INTEGER k
+ *  3. INTEGER b
+ */
+
+func bonAppetit(bill []int32, k int32, b int32) {
+	var totalSum int32 = 0
+	for _, v := range bill {
+		totalSum += v
+	}
+	paymentofAnna := (totalSum - bill[k])/2
+	if b ==paymentofAnna {
+		fmt.Println("Bon Appetit")
+	}else {
+		fmt.Println(b - paymentofAnna)
+	}
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+	firstMultipleInput := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	nTemp, err := strconv.ParseInt(firstMultipleInput[0], 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	kTemp, err := strconv.ParseInt(firstMultipleInput[1], 10, 64)
+	checkError(err)
+	k := int32(kTemp)
+
+	billTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var bill []int32
+
+	for i := 0; i < int(n); i++ {
+		billItemTemp, err := strconv.ParseInt(billTemp[i], 10, 64)
+		checkError(err)
+		billItem := int32(billItemTemp)
+		bill = append(bill, billItem)
+	}
+
+	bTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	b := int32(bTemp)
+
+	bonAppetit(bill, k, b)
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
