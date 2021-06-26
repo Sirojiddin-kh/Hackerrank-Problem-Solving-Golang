0fb283b24fc67fc754426cf8663c262cfa997d3b
Sirojiddin-Kh <sirojiddinhamidov2001@gmail.com>
2021-06-26 17:41:43 +0500

    Sequence Equation

---
+++ Sequence Equation.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'permutationEquation' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY p as parameter.
+ */
+
+func permutationEquation(p []int32) []int32 {
+	var yIndex , yValue []int32
+	for i := 1; i <= len(p) ; i++ {
+		for j := 0; j <= len(p) - 1; j++ {
+			if int32(i) == p[j] {
+				yIndex = append(yIndex, int32(j)+1)
+			}
+		}
+	}
+	for i := 0; i < len(yIndex); i++ {
+		for j := 0; j < len(p); j++ {
+			if yIndex[i] == p[j] {
+				yValue = append(yValue, int32(j) + 1)
+			}
+		}
+	}
+
+	return yValue
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	pTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var p []int32
+
+	for i := 0; i < int(n); i++ {
+		pItemTemp, err := strconv.ParseInt(pTemp[i], 10, 64)
+		checkError(err)
+		pItem := int32(pItemTemp)
+		p = append(p, pItem)
+	}
+
+	result := permutationEquation(p)
+
+	for i, resultItem := range result {
+		fmt.Fprintf(writer, "%d", resultItem)
+
+		if i != len(result) - 1 {
+			fmt.Fprintf(writer, "\n")
+		}
+	}
+
+	fmt.Fprintf(writer, "\n")
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
