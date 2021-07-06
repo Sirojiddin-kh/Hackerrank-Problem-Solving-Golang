26c44e691b9d29dac1acffe7a2ab63a252d4113a
Sirojiddin-Kh <sirojiddinhamidov2001@gmail.com>
2021-07-06 23:28:48 +0500

    Updated

---
+++ ACM ICPC Team.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+
+	var topic []string = []string{"10101", "11100", "11010", "00101"}
+
+	for i := 0; i < len(topic); i++ {
+		for j := i+1; j < len(topic); j++ {
+			if topic[i]
+		}
+	}
+}
