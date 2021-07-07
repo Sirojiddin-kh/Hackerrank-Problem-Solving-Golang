51db27c0a91097deda18eab48cbcfa5c816a03f0
Sirojiddin-Kh <sirojiddinhamidov2001@gmail.com>
2021-07-07 17:20:44 +0500

    Beautiful Triplests | Using 3 loops | Need optimization

--- BeatifulTriplets.go
+++ BeatifulTriplets.go
@@ -1,27 +1,86 @@
 package main
 
 import (
-	"fmt"
-	"math"
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
 )
 
-func main() {
-	var arr []int = []int{7, 1, 3, 4, 1, 7}
-	var allDistance []int
-    for key1, value1 := range arr {
-        for key2, value2 := range arr {
-            if value1 == value2 && key2 != key1{
-                allDistance = append(allDistance, int(math.Abs(float64(key2-key1)))) 
+/*
+ * Complete the 'beautifulTriplets' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER d
+ *  2. INTEGER_ARRAY arr
+ */
+
+func beautifulTriplets(d int32, arr []int32) int32 {
+    var count int32
+    for i := 0; i < len(arr); i++ {
+        for j := i +1; j < len(arr); j++ {
+            for k := j+1; k < len(arr); k++ {
+            if arr[j] - arr[i] == d && arr[k] - arr[j] == d {
+                count += 1
             }
         }
+        }
     }
-    var min int = allDistance[0]
+    return count
+}
 
-    for _, value := range allDistance {
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
 
-    	if value < min {
-    		min = value
-    	}
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    firstMultipleInput := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+    nTemp, err := strconv.ParseInt(firstMultipleInput[0], 10, 64)
+    checkError(err)
+    n := int32(nTemp)
+
+    dTemp, err := strconv.ParseInt(firstMultipleInput[1], 10, 64)
+    checkError(err)
+    d := int32(dTemp)
+
+    arrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+    var arr []int32
+
+    for i := 0; i < int(n); i++ {
+        arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+        checkError(err)
+        arrItem := int32(arrItemTemp)
+        arr = append(arr, arrItem)
     }
-    fmt.Println(min)
+
+    result := beautifulTriplets(d, arr)
+
+    fmt.Fprintf(writer, "%d\n", result)
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
 }
