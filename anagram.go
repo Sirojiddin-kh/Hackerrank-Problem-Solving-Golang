1690396a98f7d0f37115a69739781a30f3516ca8
Sirojiddin-Kh <sirojiddinhamidov2001@gmail.com>
2021-07-26 23:56:10 +0500

    anagram

---
+++ anagram.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+func anagram(s string) int32 {
+	if len(s)%2 != 0 {
+		return -1
+	}
+	s1 := s[len(s)/2:]
+	s2 := s[:len(s)/2]
+	// fdhlvosfpafhalll
+	m := make(map[rune]int)
+	for _, c := range s1 {
+		m[c]++
+	}
+	fmt.Println(m)
+	for _, c := range s2 {
+		m[c]--
+	}
+	t := int32(0)
+	fmt.Println(m)
+	for _, v := range m {
+		if v < 0 {
+			continue
+		}
+		t += int32(v)
+	}
+	return t
+}
+
+func main() {
+
+	s := "xaxbbbxx"
+
+	fmt.Println(anagram(s))
+}
