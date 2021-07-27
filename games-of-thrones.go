7ab92d76fd5a41f16d51db910846f5c3d4a7d38a
Sirojiddin-Kh <sirojiddinhamidov2001@gmail.com>
2021-07-27 14:54:55 +0500

    Games of Thrones

---
+++ games-of-thrones.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+func gameOfthrones(s string) string {
+
+	dict := make(map[string]uint32)
+
+	var pairs []uint32
+
+	for _, value := range s {
+
+		dict[string(value)] += 1
+
+	}
+	for _, value := range dict {
+
+		if value % 2 == 0 {
+
+			pairs = append(pairs, value)
+		}
+	}
+	fmt.Println(dict)
+	fmt.Println(pairs)
+
+	if len(pairs) != 0 {
+
+		return "YES"
+	} else {
+
+		return "NO"
+	}
+}
+
+func main() {
+
+	s := "jzrvgnywrcihhmrsdwywuuevbsygjejufdrlqpjjssqbejvjgwiaqmytgwbralmfqdqivarpsiqyrnjzlrcdfaxlyydzlrgbwzjrbbgvlyteoeezwgddgapeyufmnonjnzboajjnierwivvgbakjabqbxvnkjctybwmdgxqodimofhwhiiskjilggasrfmqiiemoeeejoqxuaudievuogiwmkpdpdvyncmcgqipfzpzzlejpzfffnetyvznyunlwzpesywxzlyywcjndotjcxecsdpsyfdwevdwtzvsmvskzdxdrqovpsxjwpbwuhuacxzxywrmsjwrouwfmmdefapdrrznavoevpdtlugdfextavapidtpgiszbtndqdjlsmhdgpjntgiudjjloecuouuspjxgmhtetajcpsrjaafelqrzwuwkqqeibcqnjjtemcgbxymxatdljvkfrgshyypzaholrjqdoyglwtkymojylufccxjdybcaitmbceeqcldbgozuejspfyrhxckvdpwmkkpmoufqhirpytjcccrjcksznfxquteedustqzjzidoiwzlabejfpbfcgeuczbiexdxnciomlcvjbijfmumdythgxcjwdtaayxpbgdpthkdkuorhjemhxunvwytbuwdstuvbzlxgxassombzqniphwmfwgiqcnjxhgbiryqpajjqvksnzacjwrqvzzldowayddgwdxnfiwyfgqsiqwrpphkoixrxwuxzxdxdclikhgrqwlypirgzaqojpfilzfmzkkcpxmzettwrewionooogczwhjhjqwiaimpfyrjtkiloetduktiqvjmeswniuxkclchkvsdhaqxhmcbwvlmqubvozkytikvvmcfjwlqzbognqhlmuyeqbzeamctwoqrbuxkqidplrxzjgkvakaqnbwpnaijpabquaclifctyauiihfjthbjjyyyyhinjldelirvptqovnyeudpplqywoxungeappgtcomxfgqvddt"
+	fmt.Println(gameOfthrones(s))
+}
